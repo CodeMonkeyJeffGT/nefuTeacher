bc639071197c27731497a1b6dfc55f7deaafb098
CodeMonkeyJeffGT <18846938586@163.com>
2018-03-10 14:30:35 +0800

    log

---
+++ log.php
@@ -0,0 +1,13 @@
+<?php
+echo '<meta http-equiv="refresh" content="1">';
+$log = json_decode(file_get_contents('log'), true);
+echo '<table>';
+for ($i = count($log) - 1; $i >= 0; $i--) {
+	echo '<tr><td>', $log[$i]['class'];
+	echo '</td><td>', $log[$i]['function'];
+	echo '</td><td>', date('m-d H:i:s', $log[$i]['time']);
+	echo '</td><td>', htmlspecialchars(json_encode($log[$i]['data']));
+	echo '</td></tr>';
+}
+echo '</table>';
+
